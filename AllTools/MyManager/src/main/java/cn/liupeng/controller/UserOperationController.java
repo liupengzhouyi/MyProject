fc6a54186f60bd9e6c4b49a3cef1f8b4a62521c5
liupeng <LIUPENG.0@outlook.com>
2019-06-21 23:37:13 +0800

    用户注册-Contorller

--- AllTools/MyManager/src/main/java/cn/liupeng/controller/UserOperationController.java
+++ AllTools/MyManager/src/main/java/cn/liupeng/controller/UserOperationController.java
@@ -1,4 +1,19 @@
 package cn.liupeng.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/baseOperation")
 public class UserOperationController {
+
+
+    @RequestMapping(path = "/userRegistration")
+    public String userRegistration() {
+        String returnValue = "userRegistration";
+        return returnValue;
+    }
+
+
+
 }
